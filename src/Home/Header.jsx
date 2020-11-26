bb0b037543ff8a4e90558dcf3edaed444824eeb9
wernerkotze <wernerkotze.rsa@gmail.com>
2020-11-26 22:57:08 +0200

    Change a lot of colors

--- src/Home/Header.jsx
+++ src/Home/Header.jsx
@@ -5,8 +5,7 @@ export default function Header(props) {
   return (
     <header {...props} id="header">
       <Link id="logo">
-        <img alt="logo" src="https://toneandrhythem.s3.us-east-2.amazonaws.com/logo.png" />
-        <img alt="Ant Design" src="https://toneandrhythem.s3.us-east-2.amazonaws.com/logo.png" />
+        <img alt="logo" src="https://toneandrhythem.s3.us-east-2.amazonaws.com/logo_tc_long.png" />
       </Link>
     </header>
   );
